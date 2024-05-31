25d5f128856563df4315149c672f7476de040227
siddhesh.go <siddhesh.go>
2024-05-31 20:57:36 +0530

    new changes css

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -6,7 +6,7 @@ interface ModalProps {
 
 export default function Modal(props: ModalProps) {
     return (
-        <div id="default-modal" tabIndex={-1} aria-hidden="true" className="bg-[rgba(0,0,0,0.4)] w-full flex justify-center items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div id="default-modal" tabIndex={-1} aria-hidden="true" className="h-screen bg-[rgba(0,0,0,0.4)] w-full flex justify-center items-center overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div className="relative p-4 w-full max-w-2xl max-h-full">
                 {props.children}
             </div>
